76fffe493da8bed8952eb21124e24328a189a456
Harvey590 <120187440+Harvey590@users.noreply.github.com>
2023-01-26 15:24:34 +0200

    Create Stand_With_Ukraine.sol

---
+++ Stand_With_Ukraine.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Solidity1_Stand_With_Ukraine_smartcontract {
+
+    // just showing the text - no spendings or interaction
+    function stand_with_Ukraine() external pure returns (string memory) {
+        return "Stand with Ukraine!";
+    }
+}
